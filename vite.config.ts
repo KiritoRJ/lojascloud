b94c435d29e58d93500e9298d51d05a8cd565d60
KiritoRJ <wanderson4d@gmail.com>
2026-02-26 00:37:36 -0300

    feat: Implement PWA and update dependencies
    This commit introduces Progressive Web App (PWA) capabilities for offline support and caching. It also refactors the main application entry point and updates the Vite configuration to include necessary PWA plugins.
    
    Additionally, dependency versions in package.json and package-lock.json have been pinned to specific versions for consistency, and unused dependencies have been removed. The client build output directory is now set to `dist/client`.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -62,14 +61,16 @@ export default defineConfig(({ mode }) => {
             enabled: true,
             type: 'module'
           }
-        }),
-        visualizer({ open: true, gzipSize: true, brotliSize: true })
+        })
       ],
 
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist/client',
       }
     };
 });
